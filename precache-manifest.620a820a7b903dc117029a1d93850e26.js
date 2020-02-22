b5d125fa27a6248f068d69be8936a44bca897fa7
olgashchekina <olya6avg@gmail.com>
2020-02-22 10:59:36 -0800

    Updates

---
+++ precache-manifest.620a820a7b903dc117029a1d93850e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6799e3de2f03d1791354160dc17d8c9a",
+    "url": "/fe-counter-app/index.html"
+  },
+  {
+    "revision": "00b6dcf883bb062faa7f",
+    "url": "/fe-counter-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "aca82bfa409cb19c1076",
+    "url": "/fe-counter-app/static/js/2.6924ed70.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/fe-counter-app/static/js/2.6924ed70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b6dcf883bb062faa7f",
+    "url": "/fe-counter-app/static/js/main.336fb647.chunk.js"
+  },
+  {
+    "revision": "e692e8f79c4a91284b42",
+    "url": "/fe-counter-app/static/js/runtime-main.b8a2ae90.js"
+  }
+]);
